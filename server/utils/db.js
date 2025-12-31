fcbe31c354cb6ca7c30d8dccaf4a7a92e9396ca7
annatsherkavska <katotoka1985@gmail.com>
2025-12-31 23:16:29 +0200

    fix variabiles

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("🟢 Database connection successful")
-    }        
-    catch (error) {
-            console.log("❌ MongoDB connection error:", err);
-            process.exit(1);
-        }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("🟢 Database connection successful");
+  } catch (error) {
+    console.log("❌ MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
 
-    }
-
-
-module.exports = connectDB;  
+module.exports = connectDB;
